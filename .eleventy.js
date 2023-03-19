38600f9a813b5b46ff6e8a06e77ac73420773e2c
Cal Jacobson <cj81499@users.noreply.github.com>
2023-03-19 16:14:08 -0500

    favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,23 @@
 const directoryOutputPlugin = require("@11ty/eleventy-plugin-directory-output");
+const faviconsPlugin = require("eleventy-plugin-gen-favicons");
 
 const SRC_DIR = "src";
+const OUTPUT_DIR = "_site";
 
 module.exports = function (eleventyConfig) {
   // https://www.11ty.dev/docs/plugins/directory-output/
   eleventyConfig.setQuietMode(true);
   eleventyConfig.addPlugin(directoryOutputPlugin);
 
-  // TODO: use eleventy image plugin https://www.11ty.dev/docs/plugins/image/
-  eleventyConfig.addPassthroughCopy(`${SRC_DIR}/assets/images`);
-  eleventyConfig.addPassthroughCopy(`${SRC_DIR}/assets/styles`);
+  // https://www.npmjs.com/package/eleventy-plugin-gen-favicons
+  eleventyConfig.addPlugin(faviconsPlugin, { outputDir: OUTPUT_DIR });
+
+  eleventyConfig.addPassthroughCopy(`${SRC_DIR}/static`);
 
   return {
     dir: {
       input: SRC_DIR,
-      output: "_site",
+      output: OUTPUT_DIR,
       includes: "_includes",
       layouts: "_layouts",
       data: "_data",
